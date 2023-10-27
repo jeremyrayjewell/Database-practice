06ead7d931430ee38e60fba73a1404d71faefa5a
jeremyrayjewell <jeremyrayjewell@gmail.com>
2023-10-27 18:29:44 -0300

    15:38

---
+++ App 1/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = required('dotenv');
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended : false}));
+
+
+// create
+
+
+// read
+
+
+// update
+
+
+// delete
